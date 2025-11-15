6d1fc372c09c7288cfe8a4da58e45cb295f49fad
MadalinaDeliu <118621308+MadalinaDeliu2@users.noreply.github.com>
2025-11-15 22:30:27 +0200

    upload basic functionality - "add" command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "finance-cli")]
+#[command(about = "Track income and expenses from the terminal", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {
+        #[arg(short, long)]
+        amount: f64,
+
+        #[arg(short, long)]
+        category: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Add { amount, category } => {
+            println!("Added transaction: {} in category '{}'", amount, category);
+        }
+    }
 }
